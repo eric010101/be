f49eabf485d6134a1ba4c5fcd1b9256154dd5f96
eric010101 <43527995+eric010101@users.noreply.github.com>
2024-06-09 19:34:30 +0800

    Update run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update
+
 sudo apt-get install -y dos2unix
 
 git clone https://github.com/eric010101/be.git
@@ -8,6 +10,7 @@ sudo cp /root/be/all.ini /root/all.ini
 sudo cp /root/be/install-allinone-OK2.sh /root/install-allinone-OK2.sh
 sudo cp /root/be/fix-https-cert.sh /root/fix-https-cert.sh
 
+dos2unix all.ini
 dos2unix install-allinone-OK2.sh
 dos2unix fix-https-cert.sh
 
@@ -17,3 +20,4 @@ sudo chmod +x fix-https-cert.sh
 
 sudo ./install-allinone-OK2.sh
 sudo ./fix-https-cert.sh
+
